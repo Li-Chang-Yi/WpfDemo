a24f3e7bb442846345ad37fa29cb58916453f0fb
Li-Chang-Yi <lichangyimail@163.com>
2024-01-25 21:29:24 +0800

    init commit
    init commit

---
+++ 3DDemo/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using HelixToolkit.Wpf;
+using System;
+using System.Windows;
+using System.Windows.Media.Media3D;
+
+namespace _3DDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //模型加载
+            string modelPath1 = AppDomain.CurrentDomain.BaseDirectory + "A.stl"; //模型地址
+
+            ModelImporter import = new ModelImporter();
+            var initGroup1 = import.Load(modelPath1);
+
+            string modelPath2 = AppDomain.CurrentDomain.BaseDirectory + "B.stl";
+
+            var initGroup2 = import.Load(modelPath2);
+            //材质
+            //GeometryModel3D geometryModel3D = initGroup.Children[0] as GeometryModel3D;
+            //DiffuseMaterial diffMat = new DiffuseMaterial(new SolidColorBrush(Colors.White));
+            //geometryModel3D.Material = diffMat;
+            //ViewPort3D进行显示
+           
+            ModelVisual3D modelVisual3d1 = new ModelVisual3D();
+            modelVisual3d1.Content = initGroup1;
+            //initGroup1.GetTransform(new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(0, 1, 0), 1)));
+            viewPort3D.Children.Add(modelVisual3d1);
+            //var axis = new Vector3D(0, 0, 1);
+            //var angle = 40;
+            //var matrix = modelVisual3d1.Transform.Value;
+            //matrix.Rotate(new Quaternion(axis, angle));
+            //modelVisual3d1.Transform = new MatrixTransform3D(matrix);
+            MV3D.Transform = new RotateTransform3D();
+            new AxisAngleRotation3D();
+
+
+
+            ModelVisual3D modelVisual3d2 = new ModelVisual3D();
+            modelVisual3d2.Content = initGroup2;
+            viewPort3D.Children.Add(modelVisual3d2);
+
+        }
+    }
+}
---
+++ GMapDemo/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using GMap.NET;
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace GMapDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Rect rc = SystemParameters.WorkArea; //获取工作区大小
+            this.Left = 0; //设置位置
+            this.Top = 0;
+            this.Width = rc.Width;
+            this.Height = rc.Height;
+            this.Map_Loaded();//加载地图
+        }
+
+        private void Map_Loaded()
+        {
+            try
+            {
+                System.Net.IPHostEntry e = System.Net.Dns.GetHostEntry("ditu.google.cn");
+            }
+            catch
+            {
+                MainMap.Manager.Mode = AccessMode.CacheOnly;
+                System.Windows.MessageBox.Show("没有可用的internet连接，正在进入缓存模式!", "提示", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            MainMap.CacheLocation = Environment.CurrentDirectory + "\\GMapCache\\"; //缓存位置
+            MainMap.MapProvider = AMapProvider.Instance; //加载高德地图
+            MainMap.MinZoom = 2;  //最小缩放
+            MainMap.MaxZoom = 17; //最大缩放
+            MainMap.Zoom = 8;     //当前缩放
+            MainMap.ShowCenter = false; //不显示中心十字点
+            MainMap.DragButton = MouseButton.Left; //右键拖拽地图
+            MainMap.Position = new PointLatLng(39.909149, 116.397486); //地图中心位置：北京
+            //MainMap.MouseLeftButtonDown += new MouseButtonEventHandler(mapControl_MouseLeftButtonDown);
+        }
+
+    }
+}
